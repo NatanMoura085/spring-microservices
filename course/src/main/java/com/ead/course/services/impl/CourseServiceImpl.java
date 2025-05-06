2a8a112b41f1bc42d04e996877a6199820104e32
Natan Moura <mouranatan933@gmail.com>
2025-05-06 05:10:39 -0300

    feat: course,lessons, methods delete

---
+++ course/src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
@@ -0,0 +1,41 @@
+package com.ead.course.services.impl;
+
+import com.ead.course.models.CourseModel;
+import com.ead.course.models.LessonModel;
+import com.ead.course.models.ModuleModel;
+import com.ead.course.repositories.CourserRepository;
+import com.ead.course.repositories.LessonRepository;
+import com.ead.course.repositories.ModuleRepository;
+import com.ead.course.services.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class CourseServiceImpl implements CourseService {
+    @Autowired
+    CourserRepository courserRepository;
+    @Autowired
+    ModuleRepository moduleRepository;
+
+    @Autowired
+    LessonRepository lessonRepository;
+
+    @Transactional
+    @Override
+    public void delete(CourseModel courseModel) {
+        List<ModuleModel> moduleModelList = moduleRepository.findAllModulesIntoCourse(courseModel.getCourseId());
+        if (!moduleModelList.isEmpty()) {
+            for (ModuleModel module : moduleModelList) {
+                List<LessonModel> lessonModels = lessonRepository.findAllLessonsIntoModule(module.getModuleId());
+                if (!lessonModels.isEmpty()) {
+                    lessonRepository.deleteAll(lessonModels);
+                }
+            }
+            moduleRepository.deleteAll(moduleModelList);
+        }
+        courserRepository.delete(courseModel);
+    }
+}
