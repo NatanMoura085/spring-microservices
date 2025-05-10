aba0e53b0c0cdc7179b19a2a056b5528a02ad15c
Natan Moura <mouranatan933@gmail.com>
2025-05-10 03:25:36 -0300

    feat: controller modules

---
+++ course/src/main/java/com/ead/course/controllers/ModuleController.java
@@ -0,0 +1,80 @@
+package com.ead.course.controllers;
+
+import com.ead.course.dtos.ModuleDTO;
+import com.ead.course.models.CourseModel;
+import com.ead.course.models.ModuleModel;
+import com.ead.course.services.CourseService;
+import com.ead.course.services.ModuleService;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class ModuleController {
+    @Autowired
+    ModuleService moduleService;
+    @Autowired
+    CourseService courseService;
+
+    @GetMapping("/courses/{courseId}/modules")
+    public ResponseEntity<List<ModuleModel>> getAllModules(@PathVariable("courseId") UUID courseId) {
+        return ResponseEntity.status(HttpStatus.OK).body(moduleService.findAllByCourse(courseId));
+    }
+
+    @GetMapping("/courses/{courseId}/modules/{moduleId}")
+
+    public ResponseEntity<Object> getOneModule(@PathVariable("courseId") UUID courseId, @PathVariable("moduleId") UUID moduleId) {
+        Optional<ModuleModel> optionalModuleModel = moduleService.findModuleIntoCourse(courseId, moduleId);
+        if (!optionalModuleModel.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("module Not found");
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(optionalModuleModel.get());
+    }
+
+    @PostMapping("/courses/{courseId}/modules")
+    public ResponseEntity<Object> saveModule(@PathVariable(value = "courseId") UUID courseId, @RequestBody @Valid ModuleDTO moduleDTO) {
+        Optional<CourseModel> optionalCourseModel = courseService.findById(courseId);
+        if (!optionalCourseModel.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Not found");
+        }
+        var moduleModel = new ModuleModel();
+        BeanUtils.copyProperties(moduleDTO, moduleModel);
+        moduleModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
+        moduleModel.setCourse(optionalCourseModel.get());
+        return ResponseEntity.status(HttpStatus.CREATED).body(moduleService.save(moduleModel));
+
+    }
+
+    @DeleteMapping("/courses/{courseId}/modules/{moduleId}")
+    public ResponseEntity<Object> deleteModule(@PathVariable(value = "courseId") UUID courseId, @PathVariable(value = "moduleId") UUID moduleId) {
+        Optional<ModuleModel> optionalModuleModel = moduleService.findModuleIntoCourse(courseId, moduleId);
+        if (!optionalModuleModel.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Module Not Found");
+        }
+        moduleService.delete(optionalModuleModel.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Module deleted successfully");
+    }
+
+    @PutMapping("/courses/{courseId}/modules/{moduleId}")
+    public ResponseEntity<Object> updateModule(@PathVariable(value = "courseId") UUID courseId, @PathVariable(value = "moduleId") UUID moduleId, @RequestBody @Valid ModuleDTO moduleDTO) {
+        Optional<ModuleModel> optionalModuleModel = moduleService.findModuleIntoCourse(courseId, moduleId);
+        if (!optionalModuleModel.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Module Not Found");
+        }
+        var moduleModel = optionalModuleModel.get();
+        moduleModel.setTitle(moduleDTO.getTitle());
+        moduleModel.setDescription(moduleDTO.getDescription());
+        return ResponseEntity.status(HttpStatus.OK).body(moduleModel);
+    }
+}
---
+++ course/src/main/java/com/ead/course/dtos/ModuleDTO.java
@@ -0,0 +1,13 @@
+package com.ead.course.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class ModuleDTO {
+    @NotBlank
+    private String title;
+    @NotBlank
+    private String description;
+
+}
