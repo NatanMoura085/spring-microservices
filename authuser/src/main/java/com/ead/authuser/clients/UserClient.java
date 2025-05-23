5abd90e437746d4f0e2f1e3ba2b683c508722696
Natan Moura <mouranatan933@gmail.com>
2025-05-23 16:44:22 -0300

    fix: resovle bug

--- authuser/src/main/java/com/ead/authuser/clients/UserClient.java
+++ authuser/src/main/java/com/ead/authuser/clients/UserClient.java
@@ -9,7 +9,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpStatusCodeException;
@@ -18,31 +17,44 @@ import org.springframework.web.client.RestTemplate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
-@Log4j2
 @Component
+@Log4j2
 public class UserClient {
+
     @Autowired
     RestTemplate restTemplate;
 
-    String REQUEST_URI = "http://localhost:8082";
+
+    private String REQUEST_URI = "http://localhost:8082";
 
     public Page<CourseDTO> getAllCoursesByUser(UUID userId, Pageable pageable) {
-        List<CourseDTO> searchResult  = new ArrayList<>();
-        String url = REQUEST_URI + "/courses?userId=" + userId + "&page=" + pageable.getPageNumber() + "&size=" + pageable.getPageSize() + "&sort=" + pageable.getSort().toString().replaceAll(":", ",");
-        log.debug("Request URL: {}", url);
-        log.info("Request URL: {}", url);
-        try {
-            ParameterizedTypeReference<ResponsePageDTO<CourseDTO>> responseType = new ParameterizedTypeReference<ResponsePageDTO<CourseDTO>>() {
-            };
-            ResponseEntity<ResponsePageDTO<CourseDTO>> result = restTemplate.exchange(url, HttpMethod.GET, null, responseType);
-            searchResult = result.getBody().getContent();
-            log.debug("Response Number of  Elements: {}", searchResult.size());
-        } catch (HttpStatusCodeException e) {
-            log.error("Error request /courses {}",e);
-        }
-        log.info("Ending request /courses userId {}",userId);
+        List<CourseDTO> searchResult = new ArrayList<>();
+        String sort = pageable.getSort().stream()
+                .map(order -> order.getProperty() + "," + order.getDirection())
+                .collect(Collectors.joining("&sort="));
 
-        return new PageImpl<>(searchResult);
+        String url = String.format("%s/courses?userId=%s&page=%d&size=%d&sort=%s",
+                REQUEST_URI, userId, pageable.getPageNumber(), pageable.getPageSize(), sort);
+
+        log.info("Request URL: {}", url);
+
+        try {
+            ParameterizedTypeReference<ResponsePageDTO<CourseDTO>> responseType = new ParameterizedTypeReference<>() {};
+            ResponseEntity<ResponsePageDTO<CourseDTO>> result = restTemplate.exchange(url, HttpMethod.GET, null, responseType);
+
+            ResponsePageDTO<CourseDTO> body = result.getBody();
+            if (body != null) {
+                searchResult = body.getContent();
+                log.debug("Response Number of Elements: {}", searchResult.size());
+                return new PageImpl<>(searchResult, pageable, body.getTotalElements());
+            }
+        } catch (HttpStatusCodeException e) {
+            log.error("Error request /courses {}", e);
+        }
+
+        log.info("Ending request /courses userId {}", userId);
+        return Page.empty(pageable);
     }
 }
